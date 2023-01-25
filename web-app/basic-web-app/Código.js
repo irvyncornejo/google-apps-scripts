9d1854c0c0efd239725778e4fcb5f98d4c47f32c
Irvyn Cornejo <irvyncornejo@gmail.com>
2023-01-24 23:18:28 -0600

    add folder for web app

---
+++ web-app/basic-web-app/Código.js
@@ -0,0 +1,21 @@
+const url = 'google sheet url'
+
+const doGet = (e) => {
+  //Logger.log(e.parameter);// e por el evento de ejecución que estará reflejado 
+  //return HtmlService.createHtmlOutputFromFile("index");
+  return HtmlService.createTemplateFromFile("index").evaluate();
+}
+
+const userClick = (infAlumno,correo) => {
+    const ss = SpreadsheetApp.openByUrl(url)
+    const data = ss.getSheetByName('DB')
+    data.appendRow([infAlumno.nombre,infAlumno.seccion,infAlumno.grupo, new Date(),correo])
+    Logger.log(name + ' Bienvenid@')
+    
+}
+
+// funcion para llamar a los archivos .css y .js dentro del html
+const include = (filename) => {
+    return HtmlService.createHtmlOutputFromFile(filename).getContent()
+
+}
