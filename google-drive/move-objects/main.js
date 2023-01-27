736ad928fde44ebbcaf35794e98739d926116358
Irvyn Cornejo <irvyncornejo@gmail.com>
2023-01-26 21:45:49 -0600

    add drive services class

--- google-drive/move-objects/main.js
+++ google-drive/move-objects/main.js
@@ -1,48 +1,71 @@
-const validateFolder = (folderName) => {
-  const $folders_ref = []
-  const folders = DriveApp.getFoldersByName(folderName)
-  while (folders.hasNext()){
-      let folder = folders.next()
-      $folders_ref.push(folder.getId())
+class GoogleDriveService {
+  validateFolder (folderName) {
+    const folders_ref = []
+    const folders = DriveApp.getFoldersByName(folderName)
+    while (folders.hasNext()){
+        let folder = folders.next()
+        folders_ref.push(folder.getId())
+    }
+    if(!folders_ref.length){
+      throw new Error(`Folder ${folderName} not exists`)
+    }
+    if(folders_ref.length === 2){
+      throw new Error('There are two folders with the same name')
+    } 
+    return folders_ref[0]
   }
-  if(!$folders_ref.length){
-    throw new Error(`Folder ${folderName} not exists`)
+
+  moveObjects (objects, childFolder,type) {
+    console.log(objects.hasNext())
+    while (objects.hasNext()){
+      let object = objects.next()
+      console.log(`${type} ${object.getName()}`)
+      object.moveTo(childFolder)
+    }
   }
-  if($folders_ref.length === 2){
-    throw new Error('There are two folders with the same name')
-  } 
-  return $folders_ref[0]
+
+  instantiateFolders (principalFolderId, childFolderId) {
+    const principalFolder = DriveApp.getFolderById(principalFolderId)
+    const childFolder = DriveApp.getFolderById(childFolderId)
+    const files = principalFolder.getFiles()
+    const folders = principalFolder.getFolders()
+    this.moveObjects(files, childFolder, 'file')
+    this.moveObjects(folders, childFolder, 'folder')
+  }
+
+  moveObjectsBtwFolders (principalFolderName, childFolderName) {
+    try{
+      const principalFolderId = this.validateFolder(principalFolderName)
+      const childFolderId = this.validateFolder(childFolderName)
+      this.instantiateFolders(principalFolderId, childFolderId)
+    }catch(e){
+      console.error(e)
+    }
+  }
+
+  getFiles (fileName) {
+    const files = DriveApp.getFilesByName(fileName)
+    const files_ref = []
+    while (files.hasNext()){
+      let file = files.next()
+      files_ref.push(file.getId())
+    }
+    if (!files_ref.length) throw new Error('Without documents')
+    return DriveApp.getFilesByName(fileName)
+  }
+
+  moveObjectsSimilarNames (fileName) {
+    const folderId = this.validateFolder(fileName)
+    const folder = DriveApp.getFolderById(folderId)
+    const objects = this.getFiles(fileName)
+    this.moveObjects(objects, folder, 'file')
+  }
 }
 
-const moveObjects = (objects, childFolder,type) => {
-  while (objects.hasNext()){
-    let object = objects.next()
-    console.log(`${type} ${object.getName()}`)
-    object.moveTo(childFolder)
-  }
+const main = () => {
+  const drive = new GoogleDriveService()
+  drive.moveObjectsBtwFolders('APPSCRIPT', 'APPSCRIPT_MOVE')
+  drive.moveObjectsSimilarNames('Documento sin título')
 }
-
-const instantiateFolder =  (principalFolderId, childFolderId) => {
-  const principalFolder = DriveApp.getFolderById(principalFolderId)
-  const childFolder = DriveApp.getFolderById(childFolderId)
-  const files = principalFolder.getFiles()
-  const folders = principalFolder.getFolders()
-  moveObjects(files, childFolder, 'file')
-  moveObjects(folders, childFolder, 'folder')
-}
-
-const moveObjectsExecute = (
-  principalFolderName='APPSCRIPT_MOVE', 
-  childFolderName='APPSCRIPT'
-) => {
-  try{
-    const principalFolderId = validateFolder(principalFolderName)
-    const childFolderId = validateFolder(childFolderName)
-    instantiateFolder(principalFolderId, childFolderId)
-  }catch(e){
-    console.log(e)
-  }
-}
-
 
 
