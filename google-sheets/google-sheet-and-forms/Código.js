63ad7608c17419602c18951bb25a211740177b61
Irvyn Cornejo <irvyncornejo@gmail.com>
2023-01-29 21:42:33 -0600

    Create class to send email

--- google-sheets/google-sheet-and-forms/Código.js
+++ google-sheets/google-sheet-and-forms/Código.js
@@ -1,29 +1,57 @@
-const main = (event) => {
-  console.log(event.namedValues)
-  sendEmail(event.namedValues)
-}
 
-const sendEmail = (event) => {
-  const response = GmailApp.sendEmail(event['Dirección de correo electrónico'][0], 'subject', 'body')
-  console.log(response)
-}
-
-const test = () => {
-  const t = { 'Marca temporal': [ '27/1/2023 20:40:12' ],
-  'Mensaje de prueba': [ 'test' ],
-  'Dirección de correo electrónico': [ 'irvyncornejo@gmail.com' ] }
-  console.log(t['Dirección de correo electrónico'][0])
-}
-
-const testEmail = () => {
-  //const response = GmailApp.sendEmail('irvyncornejo@gmail.com', 'subject', 'body')
-  const draft = GmailApp.createDraft('irvyncornejo@gmail.com', 'subject', 'body para checar id')
-  const response = draft.send()
-  console.log(response.getId())
-}
-
-const testGetEmail = () => {
-  const id = '185f67b6614ee1d0'
+const validateEmail = () => {
+  const id = '1d0'
   const email = GmailApp.getMessageById(id)
   console.log(email.isUnread())
 }
+
+class SendEmails{
+  constructor(event){
+    this.nameSheet = 'Registro de correos'
+    this.ss = SpreadsheetApp.getActiveSpreadsheet()
+    this.event = event
+    this.sheet = NaN
+  }
+  validateSheet () {
+    const sheets = this.ss.getSheets().map(sheet => sheet.getName())
+    if (!(sheets.includes(this.nameSheet))){
+      this.sheet = this.ss.insertSheet().setName(this.nameSheet)
+    }
+    this.sheet = this.ss.getSheetByName(this.nameSheet)
+  }
+  unpackEvent () {
+    console.info(this.event)
+    return Object.values(this.event).map(value => value[0])
+  }
+  sendEmail(){
+    const draft = GmailApp.createDraft(
+      this.event[2], 
+      'Correo de prueba', 
+      this.event[1]
+    )
+    const response = draft.send()
+    this.event.push(response.getId())
+  }
+  createRegister(){
+    this.sheet.appendRow(this.event)
+  }
+  run() {
+    try{
+      this.validateSheet()
+      this.event = this.unpackEvent()
+      this.sendEmail()
+      this.createRegister()
+    }catch(e){
+      console.error(e)
+    }
+
+  }
+}
+
+const main = () => {
+  const t = { 'Marca temporal': [ '27/1/2023 20:40:12' ],
+  'Mensaje de prueba': [ 'test' ],
+  'Dirección de correo electrónico': [ 'email@gmail.com' ] }
+  new SendEmails(t).run()
+}
+
